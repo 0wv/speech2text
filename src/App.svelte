ca8095615a4df564ae324c9711479826eb7be693
0wv <w32w64@gmail.com>
2021-07-13 21:45:12 +0900

    :recycle: split code

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,13 @@
 <script>
+	import SpeechStartButton from './components/SpeechStartButton.svelte'
 	import Tailwind from './components/Tailwind.svelte'
-	let disabled = false
-	const speech = new webkitSpeechRecognition()
-	let texts = []
-	speech.lang = 'ja_JP'
-	speech.onend = () => {
-		speech.start()
-	}
-	speech.onresult = e => {
-		speech.stop()
-		if (e.results[0].isFinal) {
-			const text = e.results[0][0].transcript
-			texts.push(text)
-			texts = texts
-		}
-	}
 	export let name;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<button
-		class="px-4 py-2"
-		{disabled}
-		on:click={() => {
-			speech.start()
-			disabled = true
-		}}
-	><pre><code>speech.start()</code></pre></button>
-	{#each texts as text}
-		<div>{text}</div>
-	{/each}
+	<SpeechStartButton />
 </main>
 
 <style>
---
+++ src/components/SpeechStartButton.svelte
@@ -0,0 +1,29 @@
+<script>
+    let disabled = false
+	const speech = new webkitSpeechRecognition()
+	let texts = []
+	speech.lang = 'ja_JP'
+	speech.onend = () => {
+		speech.start()
+	}
+	speech.onresult = e => {
+		speech.stop()
+		if (e.results[0].isFinal) {
+			const text = e.results[0][0].transcript
+			texts.push(text)
+			texts = texts
+		}
+	}
+</script>
+
+<button
+    class="px-4 py-2"
+    {disabled}
+    on:click={() => {
+        speech.start()
+        disabled = true
+    }}
+><pre><code>speech.start()</code></pre></button>
+{#each texts as text}
+    <div>{text}</div>
+{/each}
