ccc29f3d62c37d5b8fb4067b1524c0975033f3bc
0wv <w32w64@gmail.com>
2021-07-14 18:12:52 +0900

    :wrench: enable dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     ],
     enabled: production
   },
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     colors: {
       gray: colors.trueGray,
